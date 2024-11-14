835d9f1db1201bbc4a26b8ceeb093774c38b3b17
lewistwo <63572113+lewistwo@users.noreply.github.com>
2024-11-14 14:04:35 +0000

    Create password-protexted.php

---
+++ scripts/password-protexted.php
@@ -0,0 +1,35 @@
+<?php
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $password = $_POST['password'];
+
+    // Check if the password is correct
+    if ($password == "yourpassword") {
+        // Redirect to the protected page or link
+        header("Location: project1.html");
+        exit;
+    } else {
+        // Show an error message if the password is incorrect
+        $error = "Incorrect password!";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Protected Page</title>
+</head>
+<body>
+    <h2>Enter Password to Access Link</h2>
+    <form method="post">
+        <input type="password" name="password" />
+        <button type="submit">Submit</button>
+    </form>
+    <?php 
+    // Display the error message if there is one
+    if (isset($error)) { 
+        echo "<p style='color:red;'>$error</p>"; 
+    } 
+    ?>
+</body>
+</html>
