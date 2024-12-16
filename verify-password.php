71574f102bd41b167d73959dcb85fbb7738c7d10
lewistwo <63572113+lewistwo@users.noreply.github.com>
2024-12-16 13:14:02 +0000

    Update verify-password.php

--- verify-password.php
+++ verify-password.php
@@ -13,13 +13,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the password is correct
     if ($password === $correctPassword) {
         // Redirect to the protected page
-        header("Location: index.html");
+        header("Location: index.php"); // Direct to index.php now that it will load the content
         exit;
     } else {
         // Save error message to session and redirect back to the form
         $_SESSION['error'] = "Incorrect password! Please try again.";
-        header("Location: index.php"); // Updated to index.php
+        header("Location: index.php"); // Ensure the redirection is to the correct PHP file
         exit;
     }
 }
-?>
